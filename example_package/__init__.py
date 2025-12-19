ca91cbb508c393eb3e2a607d5ec97418c05e3725
Priyansh Nema <priyanshnema28@gmail.com>
2025-12-19 20:52:29 +0530

    Fixed all the 6 issues in the repository and file

---
+++ example_package/__init__.py
@@ -0,0 +1,5 @@
+"""Example package for Cloudsmith assessment."""
+
+from .example import hello_world
+
+__all__ = ["hello_world"]
--- src/example_package/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Example package for Cloudsmith assessment."""
-
-__version__ = "0.0.1" 
